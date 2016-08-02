af70313c172d445bf00b1104f5241a57194b58ce
izestrea <izestrea@gmail.com>
2016-08-02 18:55:48 +0300

    Createthe first complete test

---
+++ wptests/LoginTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using wpautomation;
+
+namespace wptests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Driver.Initialize();
+        }
+
+        [TestMethod]
+        public void Admin_User_Can_Login()
+        {
+            LoginPage.GotTo();
+            LoginPage.LoginAs("admin").WithPassword("password").Login();
+
+           Assert.IsTrue(DashboardPage.IsAt, "Failed to login");
+        }
+
+        [TestCleanup]
+        public void CLeanup()
+        {
+            Driver.Close();
+        }
+    }
+}
