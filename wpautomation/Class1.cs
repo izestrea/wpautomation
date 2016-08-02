2c2bb8544698828b47a5fec3d4dc34cd5cf34b4d
izestrea <izestrea@gmail.com>
2016-08-02 15:18:44 +0300

    First commit that include base of framework

---
+++ wpautomation/Class1.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpautomation
+{
+    public class Class1
+    {
+        public void Go()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://google.com"); 
+        }
+    }
+}
